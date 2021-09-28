b23294a91b72ea4bf8cd4ffcdfa157d29713074d
Idan Shoshana <ishoshana@microsoft.com>
2021-09-28 21:39:39 +0300

    Add comments

--- cyber-4/transfer.js
+++ cyber-4/transfer.js
@@ -1,26 +1,31 @@
-const readline = require('readline');
-const _ = require('lodash');
-const fs = require('fs');
+// imports
+const readline = require('readline'); // line by line
+const _ = require('lodash'); // javascript utils
+const fs = require('fs'); // work with files
 
+// initialize variables names
 const inputFile = './input-example';
 const outputFile = './output-example';
+const names = [];
 
+// create interface to use events (.on)
 const rl = readline.createInterface({
   input: fs.createReadStream(inputFile),
   output: process.stdout,
   terminal: false
 });
 
-const names = [];
-
+// after each line
 rl.on('line', (line) => names.push(line));
 
+// on finish reading
 rl.on('close', () => {
   const total = [...names];
-  total.push(...names.map(l => _.camelCase(l)));
+  total.push(...names.map(l => _.camelCase(l))); // Mor Revivo => morRevivo
   total.push(...names.map(l => l.replace(' ', '')));
   total.push(...total.map(l => l.toLowerCase()));
   total.push(...total.map(l => l.toUpperCase()));
 
+  // write the output
   fs.writeFile(outputFile, `${total.join('\n')}`, (err) => console.log(err));
 })
